981efba573bfca522c56908842fe08ab63daf259
Jordan Coleman <jcoleman314@gmail.com>
2019-12-08 21:07:54 -0600

    All Hw files from repo

---
+++ ExampleScriptToy.js
@@ -0,0 +1,12 @@
+
+var learning = prompt('What are you learning?');
+
+//var c = (a === 'true');
+
+//console.log('a:', a, typeof a);
+//console.log('c:', c, typeof c);
+
+if (learning === 'Javascript') {
+    console.log('Yes! You are learning ' + learning + '!'); }
+
+    console.log('done');
---
+++ script.js
@@ -0,0 +1,19 @@
+//This comment wont be read by Javascript. Single line Use
+
+/* This is for mulitple line use comments */
+
+var response;
+var name;
+var age;
+var hometown;
+var occupation;
+var adultvshow;
+var kidstvshow;
+
+var answer = prompt('Do you eat steak?');
+
+if (answer === "yes") {
+    console.log('Here\'s a steak sandwich!'); }
+   else if (answer === "no") {console.log('Here\'s a Tofu-Stir-Fry!')
+} 
+   
